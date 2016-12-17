33fdbc97c3c7350b618f95c669a6ad3de41926d1
Cendekia Pramana Putra <me@cendekiapp.com>
2016-12-17 11:55:44 +0700

    fix laravel missing token

--- src/routes/admin.php
+++ src/routes/admin.php
@@ -3,11 +3,11 @@
 $prefix = config('admin_boilerplate.url');
 $path = config('admin_boilerplate.controller_path');
 
-Route::group(['prefix' => $prefix, 'namespace' => $path], function () {
+Route::group(['prefix' => $prefix, 'namespace' => $path, 'middleware' => ['web']], function () {
     Route::get('signin', 'AuthController@getSignIn');
-    Route::post('signin', 'Admin\AuthController@postSignIn');
+    Route::post('signin', 'AuthController@postSignIn');
 });
 
-Route::group(['prefix' => $prefix, 'namespace' => $path, 'middleware' => ['admin.auth']], function() use ($path) {
+Route::group(['prefix' => $prefix, 'namespace' => $path, 'middleware' => ['admin.auth', 'web']], function() use ($path) {
 	Route::get('/',  'AdminBoilerplateController@index');
 });
